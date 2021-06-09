9c7a3dda977ff944d32f275ef8247f69f84265f7
Joshua Jainga <jjainga@gmail.com>
2021-06-09 14:13:30 -0700

    into to methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.Write("What is your name?");
+            name = Console.ReadLine();
+            Console.Write("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            SayHi(name, age);
+            
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + ", you are " + age + " years old");
+        }
+
+    }
+}
