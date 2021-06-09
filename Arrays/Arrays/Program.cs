499ef6f89ebd3cb58e651d4a9a2c6f8eaa75bc36
Joshua Jainga <jjainga@gmail.com>
2021-06-09 14:01:43 -0700

    learning basics of arrays in C#

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = {4, 8, 34, 12, 35, 6, 97, 3};
+            string[] friends = new string[5];
+
+            luckyNumbers[2] = 9000;
+
+            friends[0] = "Pickachu";
+            friends[1] = "Charizard";
+
+
+
+            Console.WriteLine(luckyNumbers[2]);
+            Console.WriteLine(friends[0]);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
