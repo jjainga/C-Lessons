dcff7c0e3151aaf79cc1dd5e712773ba4ecb939c
Joshua Jainga <jjainga@gmail.com>
2021-06-09 14:25:16 -0700

    Intro to return statement

---
+++ RetunStatement/RetunStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RetunStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum, cubedNum;
+
+            Console.Write("What number would you like to cude?");
+            userNum = Convert.ToInt32(Console.ReadLine());
+
+            cubedNum = cube(userNum);
+            Console.WriteLine(cubedNum);
+            Console.ReadLine();
+        }
+
+        static int cube(int num) 
+        {
+            double result = Math.Pow(num, 3);
+            return (int)result;
+        }
+    }
+}
