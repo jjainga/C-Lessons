33566391bf6036441b21c4dda6648e9c3bebea4b
Joshua Jainga <jjainga@gmail.com>
2021-06-10 12:25:10 -0700

    Guessing Game

---
+++ Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Banana";
+            string guess = "";
+
+            int guessCount = 0;
+            int guessLimit = 3;
+
+            bool outOfGueses = false;
+
+            while (secretWord != guess && !outOfGueses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("See if you can guess my word: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                    Console.WriteLine("Your guess count is: " + guessCount +" out of " + guessLimit);
+                }
+                else
+                {
+                    outOfGueses = true;
+                    
+                }
+            }
+            if(outOfGueses)
+            {
+                Console.WriteLine("You Loose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
