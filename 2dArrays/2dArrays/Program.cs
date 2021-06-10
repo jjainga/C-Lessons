b73a18de2a82a5c9fa831871814b2bb7a2af6eef
Joshua Jainga <jjainga@gmail.com>
2021-06-10 13:48:39 -0700

    2d arrays in C#

---
+++ 2dArrays/2dArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2dArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = {
+                {1,2,3,4,5 }, //index 0
+                {6,7,8,9,10 }, //index 1
+                {11,12,13,14,15 } // index 2
+            };
+
+            Console.WriteLine(numberGrid[2, 3]);
+            
+            Console.ReadLine();
+        }
+    }
+}
