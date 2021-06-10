a7914bff5593407f5d1dbdb2ab0cef46a3dc7eef
Joshua Jainga <jjainga@gmail.com>
2021-06-10 11:40:08 -0700

    Better Calculator

---
+++ Better Calculato/Better Calculato/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Better_Calculato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            string op;
+
+            Console.Write("Enter a number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an Operator: ");
+            op = Console.ReadLine();
+
+            Console.Write("Enter a second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Calculate(num1, num2, op);
+
+
+
+            Console.ReadLine();
+        }
+
+        static double Calculate(double num1, double num2, string op) 
+        {
+            double result;
+            if (op == "+")
+            {
+                result = num1 + num2;
+                Console.WriteLine(result);
+                return result;
+            }
+            else if (op == "-")
+            {
+                result = num1 - num2;
+                Console.WriteLine(result);
+                return result;
+            }
+            else if (op == "*")
+            {
+                result = num1 * num2;
+                Console.WriteLine(result);
+                return result;
+            }
+            else if (op =="/")
+            {
+                result = num1 / num2;
+                Console.WriteLine(result);
+                return result;
+            }
+            else
+            {
+                result = 0;
+                Console.WriteLine("Invalid Operator " + op);
+                return result;
+            }
+        }
+
+    }
+}
