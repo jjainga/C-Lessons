d9f2fabaae6d90a2a6f0c3dbb0d96ed70c41e9a6
Joshua Jainga <jjainga@gmail.com>
2021-06-10 12:06:09 -0700

    While and do while loops

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while(index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.WriteLine("Hello");
+            //do while loops check the condition after the first instance is ran 
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
