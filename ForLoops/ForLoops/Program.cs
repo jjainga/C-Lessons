907f7728bf4e35842ae205da6347b898914eac53
Joshua Jainga <jjainga@gmail.com>
2021-06-10 13:28:34 -0700

    For loops with arrays

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 5, 6, 233, 6, 32 };
+
+            for(int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadLine();
+        }
+    }
+}
