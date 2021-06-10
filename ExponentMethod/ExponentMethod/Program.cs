2c09b73b94ea0975ddd620d9b0e17c1eece2729f
Joshua Jainga <jjainga@gmail.com>
2021-06-10 13:41:48 -0700

    passing parameters to a method that loops

---
+++ ExponentMethod/ExponentMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExponentMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(4, 8));
+         
+            Console.ReadLine();
+        }
+
+        static int GetPow(int num1, int num2)
+        {
+            int result = 1;
+            for (int i = 0; i < num2; i++) {
+                result = num1 * result;
+            }
+            return result;
+        }
+    }
+}
