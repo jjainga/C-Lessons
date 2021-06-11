f2c9f9e11d7792516d76feac7e182a311adee246
Joshua Jainga <jjainga@gmail.com>
2021-06-11 15:10:07 -0700

    Getters and Setters

---
+++ GettersAndSetters/GettersAndSetters/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettersAndSetters
+{
+    class Movie
+    {
+        //public is open to everyone
+        //private is open to only code in this class
+
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        //Getters and Setters
+        //allows access to private from outside the class
+        public string Rating
+        {
+            get { return rating; }
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ GettersAndSetters/GettersAndSetters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie starWars = new Movie("Star Wars", "George Lucus", "PG");
+            //rating can only be
+            //G, PG, PG-13, R, NR
+
+            avengers.Rating = "Cat";
+            //Should return NR because of the invalid attribute after being filtered though the setter
+            Console.WriteLine(avengers.Rating);
+
+            Console.ReadLine();
+        }
+    }
+}
