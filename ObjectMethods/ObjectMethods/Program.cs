0d8edd12a56c86c09e82652aba7735b0d2a43d22
Joshua Jainga <jjainga@gmail.com>
2021-06-11 14:50:15 -0700

    understanding the basics ofobjects and methods

---
+++ ObjectMethods/ObjectMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObjectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Students student1 = new Students("Joshua", "Economics", 3.8);
+            Students student2 = new Students("Blake", "Engineering", 3.3);
+
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjectMethods/ObjectMethods/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMethods
+{
+    class Students
+    {
+        public string name;
+        public string major;
+        public double gpa;
+        public Students(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if(gpa >= 3.0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
