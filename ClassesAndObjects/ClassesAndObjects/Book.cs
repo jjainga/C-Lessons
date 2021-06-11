7c55255e8a9dacdd276a759c7f6ebd73509af8fb
Joshua Jainga <jjainga@gmail.com>
2021-06-11 14:21:24 -0700

    created a class and an object

---
+++ ClassesAndObjects/ClassesAndObjects/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Book
+    {
+        //in Solution explorer Name of program > right click > add > new item
+
+        //can create a custom data type
+        //give attributes to the class to define it
+
+        //class attributes
+
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //class is a specification for a new data type
+            //used to model complex entities
+
+            //an object is an instance of a class
+
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book1.title = "Lord of the Rings";
+            book1.author = "Tolkein";
+            book1.pages = 800;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+
+
+            Console.ReadLine();
+        }
+    }
+}
