4e2a6545aa4cbf93621f0857f28466769944ec13
Joshua Jainga <jjainga@gmail.com>
2021-06-11 15:46:03 -0700

    static classes, creating methods on the class level

---
+++ StaticMethods/StaticMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static method is a method that belongs to the class
+            Console.WriteLine(Math.Sqrt(144));
+
+            tools.SayHi("Joshua");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticMethods/StaticMethods/tools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMethods
+{
+    //creting a static class prevents an instance of it from being created
+    static class tools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
