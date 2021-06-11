8ac8a7d558792f56732fe8153916b7ca11aa7d89
Joshua Jainga <jjainga@gmail.com>
2021-06-11 15:27:51 -0700

    learning about the key word within classes

---
+++ StaticClasses/StaticClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song cantHoldUs = new Song("Can't Hold Us", "Macklemore", 300);
+            Console.WriteLine(Song.songCount);
+
+            Song wontSay = new Song("Won't Say I am In Love", "Meg", 180);
+            Console.WriteLine(Song.songCount); 
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticClasses/StaticClasses/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticClasses
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        //static attributes are not unique to each object
+        //they are the same accross all objects from the same class
+        public static int songCount = 0;
+
+        //constructor
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            //increments the song Count to the Sone contructor gets called each time a song is created
+            songCount++;
+
+        }
+        //could create a getter method to call from each object 
+        public int getSoungCount()
+        {
+            return songCount;
+        }
+    }
+}
